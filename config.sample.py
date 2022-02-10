48bc17a123e6e5690905175ae41491ee6a68a317
Alan <alanfromjapan@gmail.com>
2022-02-10 21:07:54 +0900

    Removed unused files, added a few comments

--- config.sample.py
+++ config.sample.py
@@ -1,12 +1,15 @@
 myconfig = {
+    #Path to the static files, make sure it ends with "/"
     "ROOTDIR" : "/home/alan/Git/alanWebSites/static/",
     "LOGFILE" : "/tmp/electrogeek.DEV.log",
     "HTTPPORT" : "1234",
     "CACHING" : "False",
     "ISPROD" : "False",
+
     "Template" : "template01.html",
     "EditTemplate" : "edit01.html",
     "NewTemplate" : "new01.html",
+
     "Login" : "test",
     "Password" : "test"
 }
